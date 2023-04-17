15c7b7efc04cf5ebad08b6126c630d0ee4db74e9
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-04-17 16:17:49 +0530

    InputManager.cs : Added Functionality

---
+++ Trace Trail/Assets/Scripts/InputManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    #region Events
+
+        public delegate void StartDraw();
+        public static event StartDraw OnStartDraw;
+
+        public delegate void EndDraw();
+        public static event EndDraw OnEndDraw;
+
+        public delegate void StartErase();
+        public static event StartErase OnStartErase;
+
+        public delegate void EndErase();
+        public static event EndErase OnEndErase;
+
+    #endregion
+
+
+    private MouseControls mousecontrols;
+
+    private void Awake()
+    {
+        mousecontrols = new MouseControls();
+    }
+
+    private void OnEnable()
+    {
+        mousecontrols.Enable();
+    }
+
+    private void OnDisable()
+    {
+        mousecontrols.Disable();
+    }
+
+    void Start()
+    {
+        mousecontrols.Mouse.Click.started += _ => { if(OnStartDraw != null) OnStartDraw(); };
+        mousecontrols.Mouse.Click.canceled += _ => { if(OnEndDraw != null ) OnEndDraw (); };
+        mousecontrols.Mouse.Erase.started += _ => { if(OnStartErase != null) OnStartErase(); };
+        mousecontrols.Mouse.Erase.canceled += _ => { if(OnEndErase != null) OnEndErase(); };
+
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public float GetZoom()
+    {
+        return mousecontrols.Mouse.Zoom.ReadValue<float>();
+    }
+    public Vector2 GetmousePosition()
+    {
+        return mousecontrols.Mouse.Positions.ReadValue<Vector2>();
+    }
+}
+
