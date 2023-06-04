f97df05d9df01090b2201854c25191a5ba803929
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-06-04 16:52:45 +0530

    Player.cs: Added player movement logic

--- Trace Trail/Assets/Scripts/Player.cs
+++ Trace Trail/Assets/Scripts/Player.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerControls playercontrols;
+    private Rigidbody2D rb;
+    private Vector3 startingPosition;
+
+    private bool playing = false;
+
+
+    private void Awake()
     {
-        
+        playercontrols = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playercontrols.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playercontrols.Disable();
+    }
+
+    void Start()
+    {
+        playercontrols.Player.Space.performed +=_=> StartGame();   
+    }
+
+    private void StartGame()
+    {
+        playing = !playing;
+
+        if(playing)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+        else
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+            transform.position = startingPosition;
+        }
     }
 }
