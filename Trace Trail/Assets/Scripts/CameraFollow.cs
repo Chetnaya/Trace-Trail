d4c8cbd16c68fdf2a9844f86c316df07bf219e83
Chetnaya Kalyan <chetnaya.kalyan@gmail.com>
2023-06-05 16:42:42 +0530

    CameraFollow.cs: Follow functionality

--- Trace Trail/Assets/Scripts/CameraFollow.cs
+++ Trace Trail/Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Follow Parameters")]
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f,2.25f, -1.5f);
+
+    [SerializeField, Range(0.01f,1f)]
+    private float smoothSpeed = 0.125f;
+
+    //For smooth Damp function
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
     {
-        
+        Vector3 desiredPosition = target.position + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
